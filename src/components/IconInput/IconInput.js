8bf2be6130eead2e7738f5785cb6ba8dcb20a2b4
Jules Blom <julesblom@gmail.com>
2022-07-27 17:58:06 +0200

    Corrections iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -4,11 +4,37 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 
 import Icon from "../Icon";
-// import VisuallyHidden from "../VisuallyHidden";
+import VisuallyHidden from "../VisuallyHidden";
 
-const StyledInput = styled.input`
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
+
+const TextInput = styled.input`
   color: ${COLORS.gray700};
+  border: none;
+  border-bottom: 1px solid black;
   font-weight: 400;
+  height: 1.5rem;
+  padding-left: 24px;
+  outline-offset: 2px;
+  width: var(--width);
+
+  &:hover {
+    font-weight: 700;
+    color: ${COLORS.black};
+  }
 
   &::placeholder {
     color: ${COLORS.gray500};
@@ -16,21 +42,46 @@ const StyledInput = styled.input`
   }
 `;
 
-const Container = styled.input`
-  width: ${(props) => props.width}px;
-  border-bottom: 1px solid black;
-  font-size: ${({ size }) => (size === "large" ? "18px" : "14px")};
+const Container = styled.label`
+  display: block;
+  position: relative;
+  font-size: ${(props) => STYLES[props.size]}px;
 `;
 
-const StyledIcon = styled.Icon`
-  height: ${({ size }) => (size === "large" ? "24px" : "16px")};
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
 `;
 
-const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+const IconInput = ({
+  label,
+  icon = "search",
+  width = 250,
+  size,
+  placeholder,
+  ...props
+}) => {
+  const id = label;
+  const styles = STYLES[size];
+
   return (
-    <Container>
-      <StyledIcon id="search" />
-      <StyledInput placeholder={placeholder} />
+    <Container style={{ "--width": width + "px" }}>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <TextInput
+        style={{
+          "--width": width + "px",
+          "--border-thickness": styles.borderThickness + "px",
+        }}
+        id={id}
+        placeholder={placeholder}
+        {...props}
+      />
     </Container>
   );
 };
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -19,21 +19,24 @@ const Background = styled.div`
   padding: ${(props) => props.size === "large" && "4px"};
 `;
 
+// in solution: make map of all variant properties
+// in solution: use the css custom properties approach by defining those in style
 const Bar = styled.div`
   background-color: ${COLORS.primary};
   width: ${(props) => `${props.value}%`};
   height: 100%;
   border-top-left-radius: 4px;
   border-bottom-left-radius: 4px;
-  border-bottom-right-radius: ${({ value }) => (value == 100 ? "4px" : "0")};
-  border-top-right-radius: ${({ value }) => (value == 100 ? "4px" : "0")};
+  border-bottom-right-radius: ${({ value }) => (value > 99 ? "4px" : "0")};
+  border-top-right-radius: ${({ value }) => (value > 99 ? "4px" : "0")};
+  overflow: hidden; 
 `;
 
 const ProgressBar = ({ value, size }) => {
   return (
     <Background size={size}>
       <Bar role="progressbar" aria-valuenow={value} value={value} />
-      <VisuallyHidden>{value}</VisuallyHidden>
+      <VisuallyHidden>{value}%</VisuallyHidden>
     </Background>
   );
 };
